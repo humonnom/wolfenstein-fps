b6e98ee0b1b6e71ee18bc096a7bbf802d0cfa8c5
juepark <juepark@student.42seoul.kr>
2021-03-26 22:39:05 +0900

    [save before submit]

--- bonus/parse/parse_tools_bonus.c
+++ bonus/parse/parse_tools_bonus.c
@@ -6,7 +6,7 @@
 /*   By: juepark <juepark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 21:45:13 by juepark           #+#    #+#             */
-/*   Updated: 2021/03/20 12:05:10 by jackjoo          ###   ########.fr       */
+/*   Updated: 2021/03/26 07:51:22 by jackjoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ int		parse_resolution(t_info *info, char *line, int *i)
 	(*i)++;
 	info->win.x = ft_atoiskip(line, i);
 	info->win.y = ft_atoiskip(line, i);
-	if (info->win.x > IMAC_X)
-		info->win.x = IMAC_X;
-	if (info->win.y > IMAC_Y)
-		info->win.y = IMAC_Y;
+	if (info->win.x > MAC_X)
+		info->win.x = MAC_X;
+	if (info->win.y > MAC_Y)
+		info->win.y = MAC_Y;
 	ft_spaceskip(line, i);
 	if (info->win.x <= 0 || info->win.y <= 0 || line[*i] != '\0')
 		return (RES_INV);
